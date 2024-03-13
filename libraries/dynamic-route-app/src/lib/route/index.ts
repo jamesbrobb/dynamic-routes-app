88ba645d3baddf6cfea6e0893b163c15391d5883
jamesbrobb <jamesbenrobb@gmail.com>
2024-03-13 15:25:34 +0000

    deleted dynamic route classes and types from dra lib

--- libraries/dynamic-route-app/src/lib/route/index.ts
+++
@@ -1,6 +0,0 @@
-export {CONTENT_RESOLVER} from "./route.resolvers";
-export * from "./config/route-config.types";
-export * from "./config/route-config.type-guards";
-export * from "./config/route-config.providers";
-export * from "./route.manager";
-export * from "./route.providers";
